76573dd24b5c394306ed5bcc00ad3ae9e361d71a
Tdude <tibbemail@gmail.com>
2025-02-20 23:19:32 +0100

    Some more endpoints

---
+++ lib/api.ts
@@ -0,0 +1,8 @@
+// lib/api.ts
+const API_URL = process.env.NEXT_PUBLIC_WORDPRESS_API_URL;
+
+export async function getPosts() {
+  const res = await fetch(`${API_URL}/wp-json/wp/v2/posts?_embed`);
+  if (!res.ok) throw new Error("Failed to fetch posts");
+  return res.json();
+}
