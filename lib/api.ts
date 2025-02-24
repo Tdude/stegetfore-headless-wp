684f5f1f3b78c248a850146a1411e7cf286045e9
Tdude <tibbemail@gmail.com>
2025-02-24 16:50:21 +0100

    New endpoint for Homepage special stuff to fetch at once

--- lib/api.ts
+++ lib/api.ts
@@ -6,3 +6,15 @@ export async function getPosts() {
   if (!res.ok) throw new Error("Failed to fetch posts");
   return res.json();
 }
+
+export async function getHomepageData() {
+  const res = await fetch(
+    `${process.env.NEXT_PUBLIC_WORDPRESS_API_URL}/wp-json/startpage/v1/homepage-data`
+  );
+
+  if (!res.ok) {
+    throw new Error("Failed to fetch homepage data");
+  }
+
+  return res.json();
+}
