d9de7bfbf019969b154efcbcb059dfdbb8e399cd
Tdude <tibbemail@gmail.com>
2025-05-21 22:28:31 +0200

    Add colorpicker for admin to design the frontend

--- inc/admin-ui.php
+++ inc/admin-ui.php
@@ -10,15 +10,51 @@ function enqueue_global_admin_scripts() {
         '1.0.1',
         true
     );
+    
+    // Load admin CSS
+    wp_enqueue_style(
+        'steget-global-admin-css',
+        get_template_directory_uri() . '/inc/css/admin.css',
+        [],
+        '1.0.1'
+    );
+    
+    // Always load color picker and central admin JS for all post edit screens
+    $screen = get_current_screen();
+    if ($screen && in_array($screen->base, ['post', 'page'])) {
+        wp_enqueue_style('wp-color-picker');
+        wp_enqueue_script('wp-color-picker');
+        wp_enqueue_script(
+            'steget-admin-color-picker',
+            get_template_directory_uri() . '/inc/js/admin-color-picker.js',
+            ['jquery', 'wp-color-picker'],
+            filemtime(get_template_directory() . '/inc/js/admin-color-picker.js'),
+            true
+        );
+    }
 }
 add_action('admin_enqueue_scripts', 'enqueue_global_admin_scripts');
 
-add_action('admin_enqueue_scripts', function($hook_suffix) {
-    global $post_type;
-    // Always load color picker for page post type (Selling Points)
-    if ($post_type === 'module' || $post_type === 'page') {
-        wp_enqueue_style('wp-color-picker');
-        wp_enqueue_script('wp-color-picker');
+add_action('admin_enqueue_scripts', function($hook) {
+    // Only on post/page/module edit screens
+    if (in_array($hook, ['post.php', 'post-new.php'])) {
+        wp_enqueue_script(
+            'selling-points-admin',
+            get_template_directory_uri() . '/inc/js/selling-points-admin.js',
+            ['jquery'],
+            filemtime(get_template_directory() . '/inc/js/selling-points-admin.js'),
+            true
+        );
+        wp_localize_script('selling-points-admin', 'stegetSellingPointsAdmin', [
+            'labels' => [
+                'selling_point' => __('Selling Point', 'steget'),
+                'title' => __('Title', 'steget'),
+                'description' => __('Description', 'steget'),
+                'icon' => __('Icon', 'steget'),
+                'color' => __('Color', 'steget'),
+                'remove' => __('Remove', 'steget'),
+            ]
+        ]);
     }
 });
 
