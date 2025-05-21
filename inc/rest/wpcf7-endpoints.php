4e470b854040c9c5319cf25702b82df0fb82a596
Tdude <tibbemail@gmail.com>
2025-05-21 22:32:38 +0200

    Safer CORS settings

--- inc/rest/wpcf7-endpoints.php
+++ inc/rest/wpcf7-endpoints.php
@@ -350,7 +350,7 @@ add_action('rest_api_init', function() {
     remove_filter('rest_pre_serve_request', 'rest_send_cors_headers');
 
     add_filter('rest_pre_serve_request', function($value) {
-        header('Access-Control-Allow-Origin: *');
+        // header('Access-Control-Allow-Origin: *'); // Commented out to allow global CORS settings to take precedence
         header('Access-Control-Allow-Methods: POST, GET, OPTIONS, PUT, DELETE');
         header('Access-Control-Allow-Credentials: true');
         header('Access-Control-Allow-Headers: Origin, X-Requested-With, Content-Type, Accept');
